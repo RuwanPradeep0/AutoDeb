08e21baaeffae6f19eb5c649a92411444a5da963
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-09-23 19:42:53 +0530

    nativewind config

---
+++ frontend/app/index.jsx
@@ -0,0 +1,11 @@
+import {StatusBar} from 'expo-status-bar'
+import {StyleShet , Text ,View} from 'react-native'
+
+export default function App(){
+    return(
+        <View>
+            <Text> Open App</Text>
+            <StatusBar style='auto' />
+        </View>
+    )
+}
