778e306fa55ace36d185696985279aa574f38cfe
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-09-26 11:23:09 +0530

    bottom bar

--- frontend/constants/icons.js
+++ frontend/constants/icons.js
@@ -1,13 +1,13 @@
 import plus from "../assets/icons/plus.png";
 import profile from "../assets/icons/profile.png";
-import chatbot from "../assets/icons/chatbot.png"
+import chat from "../assets/icons/chat.png"
 import reminders from "../assets/icons/reminders.png"
 import report from "../assets/icons/report.png"
 
 export default {
     plus,
     profile,
-    chatbot,
+    chat,
     reminders,
     report
 }
